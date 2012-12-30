14675ace2d8e31d3c87a71cf88c96e728b81922b
Nicolas Wormser <nicolas.wormser@gmail.com>
2012-12-30 19:42:48 +0100

    removed redundant code

--- spec/catalog_spec.rb
+++ spec/catalog_spec.rb
@@ -1,5 +1,4 @@
 require File.dirname(__FILE__) + '/spec_helper'
-set :environment, :test
  
 describe "Catalog Test" do
   include Rack::Test::Methods
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,6 +8,3 @@ require 'sinatra'
 require 'rspec'
 require 'rack/test'
  
-RSpec.configure do |conf|
-  conf.include Rack::Test::Methods
-end
