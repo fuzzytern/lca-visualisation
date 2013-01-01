301ff96b9dd84f0a04b60ca13a22e7d4aec5c245
Nicolas Wormser <nicolas.wormser@gmail.com>
2013-01-01 18:35:34 +0100

    some wip

--- api.rb
+++ api.rb
@@ -1,41 +1,37 @@
 require 'json/ext'
 require 'httparty'
 
-@db = "http://localhost:5984/argos"
 
 class Hypertopic
   include HTTParty
   base_uri 'http://localhost:5984/argos'
+
+  def self.rest_database(uri)
+    base_uri uri
+  end
+
+  def self.get_url(path)
+    "#{base_uri}#{path}"
+  end
+
+  def self.get_all(query)
+    self.class.get('/_all_docs')
+    # - TODO
+  end
+
+  ## Update the attributes of an element
+  #def put(json)
+  #  Hypertopic.put(@db, json)
+  #end
+
+  ## Add an element to the database
+  ## will give you back an id
+  #def post(json)
+  #  Hypertopic.post(@db, json)
+  #end
+
+  #def delete(json)
+  #  Hypertopic.delete(@db, json)
+  #end
 end
 
-def rest_database(base_url)
-  @db = base_url
-end
-
-def get_url
-  @db
-end
-
-def get_url(path)
-  # - TODO
-end
-
-def get_all(query)
-  # - TODO
-end
-
-# Update the description of a proposal, replace title
-# or description
-def put(json)
-  Hypertopic.put(@db, json)
-end
-
-# Add an element to the database, title and description
-# will give you back an id
-def post(json)
-  Hypertopic.post(@db, json)
-end
-
-def delete(json)
-  Hypertopic.delete(@db, json)
-end
--- spec/catalog_spec.rb
+++ spec/catalog_spec.rb
@@ -8,8 +8,9 @@ describe "Catalog Test" do
   end
  
   # Do a root test
-  it "should respond to /" do
+  it "displays the catalog" do
     get '/'
     last_response.should be_ok
+    last_response.body.should contain('Liste des produits')
   end
 end
