11080e808dd75b28d60d0f463fb53d1e46d123d0
Nicolas Wormser <nicolas.wormser@gmail.com>
2013-01-20 21:02:51 +0100

    more tests

--- spec/catalog_spec.rb
+++ spec/catalog_spec.rb
@@ -1,16 +1,40 @@
 require File.dirname(__FILE__) + '/spec_helper'
  
-describe "Catalog Test" do
-  include Rack::Test::Methods
- 
-  def app
-    @app ||= Sinatra::Application
+
+describe 'GET /products' do
+  include Sinatra::Test
+
+  before(:all) do
+    Httparty.put "/test" # creates the test db
   end
- 
-  # Do a root test
-  it "displays the catalog" do
+
+  after(:all) do
+    Httparty.delete "/test" # deletes the test db
+  end
+
+  before (:each) do
+    @title = "Projet TX"
+    @product_id = "product1"
+    @product_name = "BambooLine"
+
+    HTTParty.put "test/_design/#{@product_id}", {
+      :_id => @product_id,
+      :type => "product",
+      :name => "BambooLine",
+      :category => "pen",
+      :brand => "TerraPen"}.to_json,
+      :content_type => 'application/json'
+  end
+
+  it "should display the catalog" do
     get '/'
-    last_response.should be_ok
-    last_response.body.should contain('Liste des produits')
+    response.should be_ok
+    response.should have_selector("h1", :content => @title)
+  end
+
+  it "should access a product's data" do
+    get "/products/#{@product_id}"
+    response.should be_ok
+    response.should have_selector("li", :content => @product_name)
   end
 end
